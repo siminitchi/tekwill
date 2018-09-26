6a8c4eb7c3a6caef61b2e9d2c01a943c34a7a3d1
siminitchi <siminitchi.dana@gmail.com>
2018-09-26 20:20:35 +0300

    lectia 6
    training examples

---
+++ lesson6/lesson6.sql
@@ -0,0 +1,84 @@
+SELECT SESSION_ID
+FROM AD_COURSE_DETAILS;
+
+SELECT 'Test '|| q'[It's Test]'
+From  dual
+
+Select EMPLOYEE_ID,FIRST_NAME,LAST_NAME, SALARY*12 AS "SALARIU ANUAL"
+From Employees;
+
+SELECT FIRST_NAME || q'['S NAME IS  ]' || LAST_NAME
+FROM EMPLOYEES;
+
+SELECT DISTINCT EXAM_ID, EXAM_TYPE, START_DATE, NAME
+FROM AD_EXAM_DETAILS;
+
+SELECT*
+FROM AD_EXAM_DETAILS;
+
+SELECT*
+FROM AD_EXAM_TYPE;
+
+SELECT EXAM_ID
+FROM AD_EXAM_DETAILS;
+
+
+SELECT START_DATE,EXAM_TYPE AS EXAMENE
+FROM AD_EXAM_DETAILS;
+
+
+SELECT EMPLOYEE_ID
+    ,LAST_NAME  
+    ,JOB_ID
+    ,DEPARTMENT_ID
+    
+FROM EMPLOYEES
+WHERE DEPARTMENT_ID=90;
+
+
+SELECT *
+FROM EMPLOYEES
+WHERE LAST_NAME='King';
+
+SELECT *
+FROM EMPLOYEES
+WHERE LAST_NAME='KING';
+
+SELECT *
+FROM EMPLOYEES
+WHERE HIRE_DATE='09-JUL-14';
+
+
+
+
+SELECT *
+FROM EMPLOYEES 
+WHERE HIRE_DATE BETWEEN '03-JAN-14' AND '21-MAY-15'; 
+
+
+
+select *
+from employees
+where manager_id in (100,101,102);
+
+
+
+select *
+from employees
+where job_id like '___%';
+
+
+
+select * 
+from employees
+where commission_pct is not null ;
+
+
+
+
+select * 
+from employees
+where commission_pct is not null and (salary <=10000 or salary>=20000) ;
+
+
+
